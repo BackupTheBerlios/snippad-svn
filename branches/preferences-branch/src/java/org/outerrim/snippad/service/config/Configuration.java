18ed3ce079aeb735753c1e02e7e05981a7b06a80
sithlord <sithlord@b5ad054c-7ce9-0310-af1d-bb3753d9e28f>
2004-12-02 04:13:23 +0000

    Now uses PreferenceStore for storing config information

--- branches/preferences-branch/src/java/org/outerrim/snippad/service/config/Configuration.java
+++ branches/preferences-branch/src/java/org/outerrim/snippad/service/config/Configuration.java
@@ -22,32 +22,70 @@
  */
 package org.outerrim.snippad.service.config;
 
+import java.io.IOException;
+
+import org.eclipse.jface.preference.IPreferenceStore;
+import org.eclipse.jface.preference.PreferenceStore;
 import org.eclipse.swt.graphics.Point;
+import org.outerrim.snippad.SnipPadConstants;
 
 /**
  * Utility class to hold all configurable variables.
  * @author darkjedi
  */
 public class Configuration {
-    /** Location of the CSS file to use when rendering the HTML */
-    private String cssLocation = "http://snippad.berlios.de/snippad.css";
+    static public final String CSS_LOCATION = "css_location";
+    static public final String SHOW_EDITOR = "show_editor";
+    static public final String INITIAL_SIZE_X = "initial_size_x";
+    static public final String INITIAL_SIZE_Y = "initial_size_y";
+    static public final String SAVE_AS_LOCATION = "save_as_location";
     
-    /** Show the Wiki Editor by default on startup */
-    private boolean showEditor = true;
+    static private PreferenceStore store = new PreferenceStore( 
+            SnipPadConstants.PREFERENCE_FILENAME );
+
+    public Configuration() {
+        init();
+    }
     
-    /** Initial size of the window */
-    private Point initialSize = new Point( 1024, 600 );
+    public void save() 
+    throws ConfigurationException {
+        try {
+            store.save();
+        } catch( IOException E ) {
+            throw new ConfigurationException( E );
+        }
+    }
+
+    public void load()
+    throws ConfigurationException {
+        try {
+            store.load();
+        } catch( IOException E ) {
+            throw new ConfigurationException( E );
+        }
+    }
     
-    /** Default location for the Save As dialog */
-    private String saveAsLocation = System.getProperty( "user.home" );
+    public IPreferenceStore getStore() { return store; }
     
-    public String getCssLocation() { return cssLocation; }
-    public String getDefaultSaveAsLocation() { return saveAsLocation; }
-    public boolean showEditor() { return showEditor; }
-    public Point getInitialSize() { return initialSize; }
+    public String getCssLocation() { return store.getString( CSS_LOCATION ); }
+    public String getDefaultSaveAsLocation() { return store.getString( SAVE_AS_LOCATION ); }
+    public boolean showEditor() { return store.getBoolean( SHOW_EDITOR ); }
+    public Point getInitialSize() { return new Point( store.getInt( 
+            INITIAL_SIZE_X ), store.getInt( INITIAL_SIZE_Y ) ); }
     
-    public void setCssLocation( String css ) { cssLocation = css; }
-    public void setDefaultSaveAsLocation( String loc ) { saveAsLocation = loc; }
-    public void setShowEditor( boolean show ) { showEditor = show; }
-    public void setInitialSize( int x, int y ) { initialSize = new Point( x, y ); }
+    public void setCssLocation( String css ) { store.setValue( CSS_LOCATION, css ); }
+    public void setDefaultSaveAsLocation( String loc ) { store.setValue( SAVE_AS_LOCATION, loc ); }
+    public void setShowEditor( boolean show ) { store.setValue( SHOW_EDITOR, show ); }
+    public void setInitialSize( int x, int y ) { 
+        store.setValue( INITIAL_SIZE_X, x );
+        store.setValue( INITIAL_SIZE_Y, y );
+    }
+
+    private void init() {
+        store.setDefault( CSS_LOCATION, "http://snippad.berlios.de/snippad.css" );
+        store.setDefault( SHOW_EDITOR, true );
+        store.setDefault( INITIAL_SIZE_X, 1024 );
+        store.setDefault( INITIAL_SIZE_Y, 600 );
+        store.setDefault( SAVE_AS_LOCATION, System.getProperty( "user.home" ) );
+    }
 }
