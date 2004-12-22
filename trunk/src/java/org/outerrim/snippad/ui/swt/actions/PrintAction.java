806e107e67714fb42c8038fdc85c3b0836b0b394
sithlord <sithlord@b5ad054c-7ce9-0310-af1d-bb3753d9e28f>
2004-12-22 04:12:37 +0000

    Added Action for printing

---
+++ trunk/src/java/org/outerrim/snippad/ui/swt/actions/PrintAction.java
@@ -0,0 +1,47 @@
+/*
+ * PrintAction.java
+ * Created on Dec 21, 2004
+ * 
+ * Copyright (c)2004 Michael Osterlie
+ * 
+ * This file is part of snippad.
+ *
+ * snippad is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * snippad is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Foobar; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+package org.outerrim.snippad.ui.swt.actions;
+
+import org.eclipse.jface.action.Action;
+import org.outerrim.snippad.ui.swt.SnipPad;
+
+/**
+ * @author darkjedi
+ */
+public class PrintAction extends Action {
+    private SnipPad window;
+    
+    public PrintAction( SnipPad w ) {
+        window = w;
+        setText( "&Print@Ctrl+P" );
+        setToolTipText( "Print current Snip" );
+    }
+    
+    /**
+     * @see org.eclipse.jface.action.IAction#run()
+     */
+    public void run() {
+        // TODO Auto-generated method stub
+        super.run();
+    }
+}
